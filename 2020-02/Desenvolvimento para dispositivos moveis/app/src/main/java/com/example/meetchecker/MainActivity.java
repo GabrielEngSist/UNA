71e18d37c15734427b51198160dd5495404b606f
GabrielEngSist <gabriel.engsist@gmail.com>
2020-12-14 22:12:15 -0300

    feat: prova d1 substitutiva calculadora de vt

--- 2020-02/Desenvolvimento para dispositivos moveis/app/src/main/java/com/example/meetchecker/MainActivity.java
+++ 2020-02/Desenvolvimento para dispositivos moveis/app/src/main/java/com/example/meetchecker/MainActivity.java
@@ -133,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }else{
-                alert("erro");
+                alert("Não foi possível realizar a leitura");
                 MainActivity.this.setResult(0);
             }
         }else{
